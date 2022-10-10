ec41a717ac87a13e906bdf421b3a0beb2d0f3d9c
tertiomodo <itertiomodo@gmail.com>
2022-10-10 22:54:16 +0300

    feat: Supplemented block 2 new information

---
+++ js-examples/js/b1__two.js
@@ -0,0 +1,3 @@
+document.querySelectorAll('.b1__text-two').forEach(function(element) {
+  element.style.color = 'red';
+});
--- js-examples/js/block__1-third-example.js
+++
@@ -1,3 +0,0 @@
-let block = document.querySelector('.b1__block-third-example');
-
-block.classList.add('b1__translate-third-example');
